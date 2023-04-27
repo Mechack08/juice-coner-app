eacb96cf851fecafb5579c4412171245fe8244ec
8pro <mechackkobius@gmail.com>
2023-04-27 20:47:37 +0300

    Working on the location section

--- src/js/controller.js
+++ src/js/controller.js
@@ -222,3 +222,24 @@ const slide = function () {
   });
 };
 slide();
+
+// MAP
+const map = function () {
+  const map = L.map("map").setView([51.505, -0.09], 13);
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+
+  const marker = L.marker([51.5, -0.09]).addTo(map);
+
+  const circle = L.circle([51.5, -0.09], {
+    color: "red",
+    fillColor: "#f03",
+    fillOpacity: 0.5,
+    radius: 500,
+  }).addTo(map);
+};
+map();
